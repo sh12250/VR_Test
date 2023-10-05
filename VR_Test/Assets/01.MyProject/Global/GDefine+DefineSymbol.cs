a9b5e3ff7c366a972b1fbf0854106f8f74f0bd90
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-10-05 14:37:07 +0900

    오큘러스 셋팅

---
+++ VR_Test/Assets/01.MyProject/Global/GDefine+DefineSymbol.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class GDefine
+{
+    // 프로젝트에 사용할 디파인 심볼 정의 -> 팀원과 컨벤션을 정해서 사용
+    public const string DS_BUILD_PLATFORM_PC = "BUILD_PLATFORM_PC";
+}
+
+
